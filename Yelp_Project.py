709d46e4005d3f2bbc1be18e6b1b2f9e3a2c1d99
LHC2333 <32616293+LHC2333@users.noreply.github.com>
2018-05-29 13:09:24 -0400

    Update Yelp_Project.py

--- Yelp_Project.py
+++ Yelp_Project.py
@@ -28,15 +28,31 @@ text = TextInput(title="Input Abbreviation of State you want to search, ex: NY(N
 search = TextInput(title="Type the Category you want to search")
 hover = HoverTool(tooltips=[('Name','@name'),
                             ('Address','@address'),
+			    ('City','@city'),
+			    ('State','@state'),
+			    ('Zipcode','@Zip'),
                             ('Rating', '@rating'),
 			    ('Currently Opening','@status')
 			    ])
+
+hover_bar = HoverTool(tooltips=[('Rating','@x1'),
+                                ('Number of Business','@y1')
+				])
+
+hover_pie = HoverTool(tooltips=[('Percentage','@ratio')
+				])
 
 select_day = Select(
                 options=['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday','Saturday'],
                 value='Monday',
                 title='Day of the Week'
 )
+
+chosen_tools =[hover,ResetTool(),BoxSelectTool(),
+               SaveTool(),PanTool(),
+               UndoTool(),RedoTool(),ZoomInTool(),
+               ZoomOutTool()
+               ]
 
 #data
 
@@ -48,7 +64,7 @@ whole = pd.merge(data,hour, left_index=True, right_index=True)
 state_data = whole[whole.state=='NV']
 categoricaldata = state_data.categories.str.contains('')
 open_data = state_data.loc[state_data[select_day.value.lower()] != 'None']
-new_data = open_data.where(categoricaldata).dropna(thresh = 19).sort_values(by=['stars'],ascending=False)
+new_data = open_data.where(categoricaldata).dropna(thresh = 18).sort_values(by=['stars'],ascending=False)
 
 #filter_data = new_data.categories.split(';')
 
@@ -58,7 +74,10 @@ source = ColumnDataSource(data={
     'rating'  : new_data.stars.astype(str),
     'status'  : new_data.is_open.astype(str).replace({'0':'Closed','1':'Open'}),
     'name'    : new_data.name,
-    'address' : new_data.address
+    'address' : new_data.address,
+    'city'    : new_data.city,
+    'state'   : new_data.state,
+    'Zip'     :new_data.postal_code
     })
 
 histo_source = Counter(new_data.stars)
@@ -66,6 +85,17 @@ keys,values = list(histo_source.keys()), list(histo_source.values())
 source1 = ColumnDataSource(data={
             'x1' : keys,
             'y1' : values
+})
+
+total = sum(histo_source.values())
+great = histo_source[4.0]+histo_source[4.5]+histo_source[5.0]
+ratio1 = great/total
+
+pie_source1 = ColumnDataSource(data={
+                            'ratio':[ratio1, 1-ratio1],
+                            'start':[0, ratio1*2*pi],
+                            'end': [ratio1*2*pi, 2*pi],
+                            'color': ['darkgreen', 'darkred']
 })
 
 xmin, xmax = (new_data.longitude.median()-10), (new_data.longitude.median()+10)
@@ -89,8 +119,8 @@ else:
 plot = figure(title='Yelp Rating',
               x_range=(xmin, xmax),
               y_range=(ymin, ymax),
-              plot_width=700,
-              plot_height=500,
+              plot_width=500,
+              plot_height=400,
               tools=chosen_tools)
 
 
@@ -103,13 +133,19 @@ plot.circle(x = 'x',
             )
 
 
-plot1 = figure(title="Overall Rating within this Area",tools="save",plot_width = plot.plot_width,
-               plot_height = 350,background_fill_color="white")
+bar1 = figure(title="Overall Rating within this Area",tools="save",
+	      plot_width = plot.plot_width, 
+	      plot_height = 400,
+	      background_fill_color="white"
+	     )
 
-plot1.vbar(x='x1', width=0.3, bottom=0,top='y1', color='firebrick', source = source1)
+bar1.vbar(x='x1', width=0.3, bottom=0,top='y1', color='firebrick', source = source1)
 
-labels = LabelSet(x='x1', y='y1',text = 'y1',x_offset=-13.5,level = 'glyph', source=source1, render_mode='canvas')
-plot1.add_layout(labels)
+labels = LabelSet(x='x1', y='y1',text = 'y1',
+		  x_offset=-13.5,level = 'glyph', 
+		  source=source1, render_mode='canvas')
+
+bar1.add_layout(labels)
 
 plot.patches(state_xs, state_ys, fill_alpha=0.0,
           line_color="#884444", line_width=2, line_alpha=0.3)
@@ -117,6 +153,14 @@ plot.patches(state_xs, state_ys, fill_alpha=0.0,
 #plot.legend.click_policy="mute"
 plot.title.text_font_size = '16pt'
 plot.title.align = 'left'
+
+pie1 = figure(title = 'Percentage of 4 Stars and above in this category',
+              plot_width = plot.plot_width,
+              plot_height = 400,
+              tools = [hover_pie,SaveTool()])
+
+pie1.wedge(x=0, y=0, start_angle='start', end_angle='end', radius=0.7,
+        color='color', alpha=0.8, source=pie_source1)
 
 #defining the update function
 def update_value(attr, old, new):
@@ -134,8 +178,11 @@ def update_value(attr, old, new):
     'state'   : new_data1.state,
     'name'    : new_data1.name,
     'address' : new_data1.address,
+    'city'    : new_data.city,
+    'state'   : new_data.state,
+    'Zip'     :new_data.postal_code,
     'status'  : new_data1.is_open.astype(str),
-    'Day'     : new_data1.name,
+    'Day'     : new_data1.name
 
     }
 
@@ -146,9 +193,21 @@ def update_value(attr, old, new):
             'x1' : keys1,
             'y1' : values1
     }
+	
+    total1 = sum(histo_source1.values())
+    great1 = histo_source1[4.0]+histo_source1[4.5]+histo_source1[5.0]
+    ratio12 = great1/total1
+    update_data2 ={
+                    'start':[0, ratio12*2*pi],
+                    'end':[ratio12*2*pi, 2*pi],
+                    'ratio':[ratio12, 1-ratio12],
+                    'color':['darkgreen', 'darkred']
+    }
+	
 
     source.data = update_data
     source1.data = update_data1
+    pie_source1.data = update_data2
 
     xmin, xmax = (new_data1.longitude.median()-10), (new_data1.longitude.median()+10)
     ymin, ymax = (new_data1.latitude.median()-5), (new_data1.latitude.median()+5)
@@ -160,14 +219,14 @@ def update_value(attr, old, new):
     else:
         mapper = CategoricalColorMapper(palette=RdYlGn[palettes_list], factors=rating_list)
 
-    plot.title.text = 'Yelp Rating in %s on %s' % (text.value,select_day.value)
+    plot.title.text = 'Yelp Rating about %s in %s on %s' % (search.value,text.value,select_day.value)
 
 text.on_change('value',update_value)
 select_day.on_change('value',update_value)
-slider.on_change('value', update_value)
+#slider.on_change('value', update_value)
 search.on_change('value', update_value)
 #hover.on_change('value',update_value)
 
-layout = row(widgetbox(text,search,select_day), column(plot,plot1))
+layout = row(widgetbox(text,search,select_day), column(plot,row(bar1,pie1)))
 curdoc().add_root(layout)
 curdoc().title = "Yelp Rate in US"
